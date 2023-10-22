c03db017a882dafd6d6ba7e3adac6a5e79dff82a
kwendo1 <erickwendo@gmail.com>
2023-10-22 16:33:49 +0300

    first commit

--- src/main/java/com/kwendo/studentSystem/controller/StudentController.java
+++ src/main/java/com/kwendo/studentSystem/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.kwendo.studentSystem.controller;
 import com.kwendo.studentSystem.model.Student;
 import com.kwendo.studentSystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add")
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
